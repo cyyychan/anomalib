4173c3578ca09974fe87b92ba0aa837012ee15fe
siyuan.chen <siyuan.chen@smartmore.com>
2025-11-09 11:05:17 +0800

    feat: update test

--- examples/smore/test_run.py
+++ examples/smore/test_run.py
@@ -7,6 +7,8 @@ from anomalib.data import SmoreAD
 from anomalib.models import Patchcore
 from anomalib.engine import Engine
 
+# 这两个txt可以自己指定，假如训练集和测试集一直则设置成一样的即可
+# 假如是图片文件数据可以通过parse_image.py生成txt文件
 # Initialize components
 datamodule = SmoreAD("/dataset/siyuanchen/AD/datasets/Anamalib_test/train.txt", "/dataset/siyuanchen/AD/datasets/Anamalib_test/test.txt")
 model = Patchcore()
@@ -14,3 +16,6 @@ engine = Engine()
 
 # Train the model
 engine.fit(datamodule=datamodule, model=model)
+
+# Test the model
+engine.test(datamodule=datamodule, model=model)
